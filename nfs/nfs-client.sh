b22a8adfb94f03d9f7175ca763165914c9113376
Linh B Ngo <lngo@wcupa.edu>
2020-11-12 12:54:38 -0500

    Update nfs-client.sh

--- nfs/nfs-client.sh
+++ nfs/nfs-client.sh
@@ -4,7 +4,7 @@ sudo yum install -y nfs-utils
 
 for dir in scratch software home; do
   mkdir -p /$dir
-  while [ ! -d /scratch/flag ]; do
+  while [ ! -d /$dir/flag ]; do
     sudo mount -t nfs 192.168.1.1:/$dir /$dir || true
     sleep 60
   done
@@ -14,7 +14,3 @@ for dir in scratch software home; do
   echo '192.168.1.1:$dir /$dir nfs defaults 0 0' | sudo tee -a /etc/fstab
 done
 
-for i in $(ls -l /users | grep rwx | cut -d' ' -f3); do
-  USER_GROUP=`id -gn ${i}`
-  sudo usermod -d /home/$i $i
-done
