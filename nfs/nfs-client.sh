86420b8cf30d357b1ce79a9d2eb76b32b1c5a03f
Linh B Ngo <lngo@wcupa.edu>
2020-11-12 08:48:46 -0500

    Update nfs-client.sh

--- nfs/nfs-client.sh
+++ nfs/nfs-client.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 sudo yum install -y nfs-utils
+
+for dir in home scratch software; do
+  mkdir -p /$dir
+  while [ ! -d /home/flagdir ]; do
+    sudo mount 192.168.1.1:/software /software || true
+    sleep 60
+  done
+done
+
 sudo mkdir -p /scratch
 sudo mkdir -p /software
 sudo mount -t nfs 192.168.1.1:/scratch /scratch
