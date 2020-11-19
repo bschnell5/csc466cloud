9d6b59c16e0af10196e2c339ba235aef583bbc02
Linh B Ngo <lngo@wcupa.edu>
2020-11-18 20:28:11 -0500

    Update nfs-server.sh

--- nfs/nfs-server.sh
+++ nfs/nfs-server.sh
@@ -2,7 +2,7 @@
 
 sudo yum install -y nfs-utils
 
-for dir in scratch software home; do
+for dir in scratch software; do
   mkdir -p /$dir/flag
   chown nobody:nobody /$dir
   sudo chmod 755 /$dir
@@ -18,8 +18,9 @@ sudo systemctl start nfs-server
 sudo systemctl start nfs-lock
 sudo systemctl start nfs-idmap
 
-for dir in scratch software home; do
+for dir in scratch software; do
   echo "$dir 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
   echo "$dir 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+  echo "$dir 192.168.1.4(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 done
 sudo systemctl restart nfs-server
